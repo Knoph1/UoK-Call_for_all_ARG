e9c73d815682c4a64fa38a0fbbd472a10c9d586c
Felix Kiprotich <fkiprotich845@gmail.com>
2025-03-06 01:41:05 +0300

    format code

--- resources/views/partials/headercommon.blade.php
+++ resources/views/partials/headercommon.blade.php
@@ -2,27 +2,27 @@
 <header id="header" class="header">
 
 
-  <div class="top-right">
+    <div class="top-right">
 
 
 
-    <div class="header-menu">
-      <div class="navbar-brand" style="text-align:center">
-        <img src="images/home_dsh.jpeg" alt="Logo" style="width:48px;height:32px;">
+        <div class="header-menu">
+            <div class="navbar-brand" style="text-align:center">
+                <img src="images/home_dsh.jpeg" alt="Logo" style="width:48px;height:32px;">
 
-      </div> <label style="margin-top: 0.4%; margin-right: 40%; font-weight: 900; font-size: 24px;">Research Grants
-        Portal</label>
-      @guest
-      <a class="nav-link " href="{{ route('login') }}">Login</a>
+            </div> <label style="margin-top: 0.4%; margin-right: 40%; font-weight: 900; font-size: 24px;">Research Grants
+                Portal</label>
+            @guest
+                <a class="nav-link " href="{{ route('login') }}">Login</a>
 
-      <a class="nav-link" href="{{ route('register') }}">Register</a>
+                <a class="nav-link" href="{{ route('register') }}">Register</a>
 
-    @endguest
+            @endguest
 
-      @auth
-      <a class="nav-link" href="{{ route('pages.dashboard') }}">Dashboard</a>
+            @auth
+                <a class="nav-link" href="{{ route('pages.dashboard') }}">Dashboard</a>
 
-    @endauth
+            @endauth
+        </div>
     </div>
-  </div>
 </header>
